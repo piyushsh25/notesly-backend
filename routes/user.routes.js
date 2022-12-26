5ef93438c8b443aa9b329b8a72f4151b91fb1545
piyush <piyushsharmashpr@gmail.com>
2022-12-26 13:42:21 +0530

    [bug] router.use() func

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,20 +3,21 @@ var jwt = require('jsonwebtoken');
 const { formatDate, authVerify } = require("../middleware/authentication");
 const { NoteslyUsers } = require("../models/user.model");
 const router = express.Router();
-router.post("/edit", authVerify, async (req, res) => {
-    try {
-        const {
-            ...params } = req.body.user
-        const { userId } = req.user
+router.route("/edit")
+    .post(authVerify, async (req, res) => {
+        try {
+            const {
+                ...params } = req.body.user
+            const { userId } = req.user
+            console.log(params)
+            const condition = {
+                userId, ...params
+            }
+            const updateUser = await NoteslyUsers.findByIdAndUpdate({ userId: userId }, condition)
+            const [foundUser] = await NoteslyUsers.find({ userId: userId })
+            res.status(200).json({ success: true, message: notes })
+        } catch {
+            res.status(404).json({ success: false, message: "error editing user" })
 
-        const condition = {
-            userId, ...params
         }
-        const updateUser = await NoteslyUsers.findByIdAndUpdate({ userId: userId }, condition)
-        const [foundUser] = await NoteslyUsers.find({ userId: userId })
-        res.status(200).json({ success: true, message: notes })
-    } catch {
-        res.status(404).json({ success: false, message: "error editing user" })
-
-    }
-})
+    })
